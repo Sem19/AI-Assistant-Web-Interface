db911bbe392adcdbeb6673f91ed734a7a775d225
Sem19 <semkivvolodya@gmail.com>
2024-09-26 13:23:48 +0200

    add errorBoundary

--- AI-Assistant/src/App.jsx
+++ AI-Assistant/src/App.jsx
@@ -1,10 +1,48 @@
-import Test from "./pages/tabs/tabs.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./componets/error-boundary.jsx";
+import LoginPage from "./pages/login-page/login-page.jsx";
+import RegisterPage from "./pages/registration-page/registration-page.jsx";
+import ThreadViewPage from "./pages/chat-page/chat-page.jsx";
+import ThreadsPage from "./pages/main-page/main-page.jsx";
 
 function App() {
   return (
-    <div>
-      <Test />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <ErrorBoundary>
+              <LoginPage />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ErrorBoundary>
+              <RegisterPage />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ErrorBoundary>
+              <ThreadsPage />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/threads/:threadId"
+          element={
+            <ErrorBoundary>
+              <ThreadViewPage />
+            </ErrorBoundary>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
